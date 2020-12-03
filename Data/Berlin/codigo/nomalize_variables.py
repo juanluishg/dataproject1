af25a8674a09ac97c53085a9d46d3a334f3f3c2b
JuanLu <juanluishg@hotmail.es>
2020-12-03 21:08:25 +0100

    normalizar variables berlin

--- Data/Berlin/codigo/nomalize_variables.py
+++ Data/Berlin/codigo/nomalize_variables.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 import numpy as np
+import math
 
 from datetime import datetime, timedelta
 
@@ -40,26 +41,29 @@ def temperature():
 def num_precipitaciones_al_mes():
     prep = pd.read_csv("precipitaciones_por_dia.txt", sep=";")
     num_prep_dias = pd.DataFrame(columns={'id_estacion', 'fecha', 'num_dias'})
+    prep.columns = prep.columns.str.replace(' ', '')
 
-    for i in range(1963, 2020):
-        for j in range(1,13):
-            newDate = str(i)+"{:02d}".format(j)
-            num_prep_dias = num_prep_dias.append({'id_estacion': 430, 'fecha': newDate, 'num_dias':0}, ignore_index=True)
-    
-    #print(num_prep_dias.head())
+    year = []
+    month = []
+    days = []
+    for a in prep.MESS_DATUM:
+        aux = str(a)
+        year.append(aux[:4])
+        month.append(aux[4:6])
+        days.append(aux[6:])
 
-    for i, r in prep.iterrows():
-        if r[4] != 0:
-            s =  r[1]
-            year = s/10000
-            day = s%100
-            month = ((s%10000)-day)/100
-            date = datetime(year=int(year), month=int(month), day=int(day))
-            n = num_prep_dias.loc[(num_prep_dias['fecha'] == date.strftime("{%Y%m}")), "num_dias"]
-            #print(n)
-            num_prep_dias.loc[(num_prep_dias['fecha'] == date.strftime("{%Y%m}")), "num_dias"] = n + 1
+    dates = pd.DataFrame({'Year': year, 'Month': month, 'Day': days})
 
-    print(num_prep_dias.head())
+    prep = pd.concat([prep, dates], axis=1)
+
+    sum_df = prep.groupby(['Year','Month', 'Day']).agg({'RS':'sum'})
+
+    sum_df.loc[(sum_df['RS'] > 0), 'Lluvia'] = 1
+    sum_df.loc[(sum_df['RS'] == 0), 'Lluvia'] = 0
+
+    sum_df = sum_df.groupby(['Year','Month']).agg({'Lluvia':'sum'})
+
+    sum_df.to_csv('normalize/num_dias_lluvia.csv')
 
 
 def poblacion():
@@ -109,4 +113,77 @@ def arte():
 
     print(res)
 
-arte()
+"""
+year = []
+month = []
+for a in CLIMA.REF_DATE:
+    aux = str(a)
+    year.append(aux[:4])
+    month.append(aux[4:6])
+
+dates = pd.Dataframe({'Year': year, 'Month': month})
+
+CLIMA = pd.concat([CLIMA, dates], axis=1)
+"""
+
+
+def ipc ():
+    ipc = pd.read_csv('IPC-por-CA.csv', sep=";", encoding="ansi", header=0, skiprows=4)
+    ipc = ipc.rename(columns={ipc.columns[0]:'Year'})
+
+    ipc = ipc.dropna(axis=0, how='any')
+    #print(ipc.tail())
+
+    #(ln(n)-ln(n-1))/n-(n-1)
+    #n1 = n
+    #n2 = n-1
+
+    n2 = ipc.Berlin[0]
+    aux = []
+    aux.append(0)
+    for i in range(1, len(ipc.Berlin)):
+        n1 = ipc.Berlin[i]
+        if n1 == n2:
+            aux.append(0)
+        else:
+            res = (math.log(n1)-math.log(n2))/(n1-n2)
+            aux.append(res)
+            n2 = ipc.Berlin[i]
+
+    variacion = pd.DataFrame({'Variacion': aux})
+
+    res = pd.concat([ipc, variacion], axis=1)
+
+    res = res[['Year', 'Berlin', 'Variacion']]
+
+    res.to_csv("normalize/ipc_con_variacion.csv")
+
+
+def pib():
+    pib = pd.read_csv('PIB_Por_Regiones.csv', sep=";", encoding="ansi", header=0, skiprows=6)
+    pib = pib.rename(columns={pib.columns[0]:'Year'})
+    pib = pib.dropna(axis=0, how='any')
+
+    ppp = pd.read_csv("DP_LIVE_03122020201426740.csv", sep=",")
+
+    berlin_pib = pib[['Year', 'Berlin']]
+
+    berlin_pib['Year'] = berlin_pib['Year'].astype(int)
+
+    berlin_pib = berlin_pib.loc[(berlin_pib['Year']>=2000)]
+
+    berlin_pib = berlin_pib.reset_index()
+
+    ppp = ppp.loc[(ppp['LOCATION'] == 'DEU')]
+
+    ppp = ppp[['TIME', 'Value']]
+
+    ppp = ppp.rename(columns={'TIME':'Year'})
+
+    newDf = pd.merge(berlin_pib, ppp, on='Year')
+
+    newDf['ppp'] = newDf['Berlin'] * newDf['Value']
+
+    newDf = newDf[['Year','Berlin','Value','ppp']]
+
+    newDf.to_csv("normalize/pib_in_ppp.csv")
